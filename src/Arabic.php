8ad63a5cb0f33e984f046cea856bcd445166cb81
Khaled Al-Shamaa <khaled.alshamaa@gmail.com>
2023-07-24 12:57:33 +0300

    Fix reported issue in #64

--- src/Arabic.php
+++ src/Arabic.php
@@ -2396,8 +2396,12 @@ class Arabic
                     $output .= '&#x' . $this->arGlyphs[$crntChar . $nextChar][0] . ';';
                 }
                 if ($prevChar == 'ل') {
-                    $tmp_form = (isset($this->arGlyphs[$chars[$i - 2]]['prevLink']) &&
-                                 $this->arGlyphs[$chars[$i - 2]]['prevLink'] == true) ? 3 : 2;
+					if(isset($chars[$i - 2])) {
+						$tmp_form = (isset($this->arGlyphs[$chars[$i - 2]]['prevLink']) &&
+									 $this->arGlyphs[$chars[$i - 2]]['prevLink'] == true) ? 3 : 2;
+					} else {
+						$tmp_form = 2;
+					}
                     $output .= '&#x' . $this->arGlyphs[$prevChar][$tmp_form] . ';';
                     $i--;
                 }
